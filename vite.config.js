dc60334ca75c28bdb4c99ff331bd3c1b8f53a331
Gregor Majcen <majcn.m@gmail.com>
2024-06-11 21:51:59 +0200

    add proxy for easier development

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/abc': {
+        target: 'https://note-b7n.pages.dev/abc',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/abc/, ''),
+      },
+    }
+  },
   build: {
     rollupOptions: {
       output: {
