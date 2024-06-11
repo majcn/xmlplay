0c3843b8ecf9019b241a3dca4eab20263ce32477
Gregor Majcen <majcn.m@gmail.com>
2024-06-11 21:44:23 +0200

    access_token don't live long enough

--- functions/abc/[name].abc.js
+++ functions/abc/[name].abc.js
@@ -1,16 +1,35 @@
-const DROPBOX_DOWNLOAD_API_URL = "https://content.dropboxapi.com/2/files/download";
+async function getDropboxAccessToken(refresh_token, access_key, secret_key) {
+  const response = await fetch("https://api.dropbox.com/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      "grant_type": "refresh_token",
+      "refresh_token": refresh_token,
+      "client_id": access_key,
+      "client_secret": secret_key,
+    }),
+  });
+
+  return response.json()["access_token"];
+}
 
 export async function onRequest(context) {
-  const response = await fetch(DROPBOX_DOWNLOAD_API_URL,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/plain",
-        "Authorization": `Bearer ${context.env.ACCESS_TOKEN}`,
-        "Dropbox-API-Arg": `{"path": "/${context.params.name}.abc"}`,
-      }
-    }
+  const accessToken = await getDropboxAccessToken(
+    context.env.DROPBOX_REFRESH_TOKEN,
+    context.env.DROPBOX_ACCESS_KEY,
+    context.env.DROPBOX_SECRET_KEY,
   );
+
+  const response = await fetch("https://content.dropboxapi.com/2/files/download", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+      "Authorization": `Bearer ${accessToken}`,
+      "Dropbox-API-Arg": `{"path": "/${context.params.name}.abc"}`,
+    },
+  });
 
   const data = await response.text();
 
